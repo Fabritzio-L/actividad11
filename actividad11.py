ef5d5cdc71bf9502abf1cd06a8478654703b36d4
fabri <fabritzioalejandrol@gmail.com>
2025-07-31 09:52:39 -0600

    Mostrar propietarios y vehiculos

--- actividad11.py
+++ actividad11.py
@@ -2,26 +2,37 @@ propietarios ={}
 cantidad_propietarios=int(input("¿Cuantos propietarios desea ingresar?: "))
 for i in range(cantidad_propietarios):
     print(f"\nPropietario {i+1}")
-    NIT = int(input("Ingrese numero de NIT"))
+    nit = int(input("Ingrese numero de NIT: "))
     nombre= input("Ingrese nombre completo del propietario: ")
     telefono= input("Ingrese numero de contacto: ")
     cantidad_vehiculos= int(input("Ingrese cantidad de vehiculos que posee: "))
+    vehiculos={}
     for j in range(cantidad_vehiculos):
-        placa = input("Ingrese numero de placa del vehiculo: ")
+        print(f"\nVehiculo #{j+1}")
+        placa = input("Ingrese numero de placa del vehiculo: ").upper()
         marca= input("Ingrese marca del vehiculo: ")
         modelo = input("Ingrese modelo del vehiculo: ")
         año = int(input("Ingrese año del vehiculo: "))
         impuesto_pagado = input("¿Pagó el impuesto (Si/No)?: ").lower()
-    propietarios[NIT]= {
+        vehiculos[placa]={
+            "marca": marca,
+            "modelo": modelo,
+            "año": año,
+            "impuesto": impuesto_pagado
+        }
+    propietarios[nit]= {
         "nombre": nombre,
         "telefono": telefono,
-        "vehiculos": [
-            {
-                "placa": placa,
-                "marca": marca,
-                "modelo": modelo,
-                "año": año,
-                "impuesto pagado": impuesto_pagado
-            }
-        ]
     }
+print("\nRegistro de propietarios y sus vehiculos")
+for nit, datos in propietarios.items():
+    print(f"\nNIT: {nit}")
+    print(f"Nombre: {datos["nombre"]}")
+    print(f"Numero de contacto: {datos["telefono"]}")
+    for placa, data in vehiculos.items():
+        print("\nVehiculo")
+        print(f"Placa: {placa}")
+        print(f"Marca: {data["marca"]}")
+        print(f"Modelo: {data["modelo"]}")
+        print(f"Año: {data["año"]}")
+        print(f"Ha pagado el impuesto: {data["impuesto"]}")
