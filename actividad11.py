418badad1d5044138927e43cfe1c82c8649fba4e
fabri <fabritzioalejandrol@gmail.com>
2025-07-31 18:23:09 -0600

    Validar que el nit y la placa no se repita

--- actividad11.py
+++ actividad11.py
@@ -2,18 +2,38 @@ propietarios ={}
 cantidad_propietarios=int(input("¿Cuantos propietarios desea ingresar?: "))
 for i in range(cantidad_propietarios):
     print(f"\nPropietario {i+1}")
-    nit = input("Ingrese numero de NIT: ")
+    while True:
+        nit = input("Ingrese numero de NIT: ")
+        if nit in propietarios:
+            print("NIT ya ingresado")
+        else:
+            break
     nombre= input("Ingrese nombre completo del propietario: ")
     telefono= input("Ingrese numero de contacto: ")
     cantidad_vehiculos= int(input("Ingrese cantidad de vehiculos que posee: "))
     vehiculos={}
     for j in range(cantidad_vehiculos):
         print(f"\nVehiculo #{j+1}")
-        placa = input("Ingrese numero de placa del vehiculo: ").upper()
+        while True:
+            placa = input("Ingrese numero de placa del vehiculo: ").upper()
+            repetida = False
+            for datos in propietarios.values():
+                if placa in datos["vehiculos"]:
+                    repetida = True
+                    break
+            if repetida:
+                print("Placa ya ingresada")
+            else:
+                break
         marca= input("Ingrese marca del vehiculo: ")
         modelo = input("Ingrese modelo del vehiculo: ")
         año = int(input("Ingrese año del vehiculo: "))
-        impuesto = input("¿Pagó el impuesto (Si/No)?: ").lower()
+        while True:
+            impuesto = input("¿Pagó el impuesto (Si/No)?: ").lower()
+            if impuesto not in ["si","sí","no"]:
+                print("Ingrese solo si o no")
+            else:
+                break
         vehiculos[placa]={
             "marca": marca,
             "modelo": modelo,
@@ -49,5 +69,7 @@ for datos in propietarios.values():
             pagados+=1
         else:
             no_pagados+=1
+print("-"*20)
 print(f"\nVehiculos con impuesto pagado: {pagados}")
 print(f"Vehiculos sin el impuesto pagado: {no_pagados}")
+print("-"*20)
